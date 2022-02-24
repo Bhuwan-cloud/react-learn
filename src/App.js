d08ee25ac352225b83c33c55e9e243187cb4c094
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2022-02-24 10:43:03 +0545

    dark mode in about component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       {/* <NavBar ></NavBar> */}
-      <NavBar title="Text Utils" aboutText="About Us"></NavBar>
+      <NavBar title="Text Analyzer" aboutText="About Us"></NavBar>
       <div className="container">
         <TextForm heading="Enter your Text" />
+        {/* <About/> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,22 +13,40 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleClearClick = () => {
+        let newText = '';
+        setText(newText)
+    }
+
+    // const handleEmailExtract = () => {
+    //     var regex = /<(.*)>/g;
+    //     let  etext = regex.exec(text);
+    //     console.log(etext);
+
+    // }
+
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
         //  use state hook  
-    const [text, setText] = useState('Enter text Here new value ');
+    const [text, setText] = useState(' ');
     
     return (
         <>
         <div className="form-control">
             
             <h3>{props.heading} </h3>
-            <form className="mb-3">
-                <textarea ClassName="form-control my-4" value={text} onChange={handleOnChange} id="myBox" rows="10" ></textarea>
+            <form>
+                <div className="mb-3">
+                    <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
             </form>
-            <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Upper Case  </button>
-            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lower Case  </button>
+       
+            <button className="btn btn-success mx-2 my-2" onClick={handleUpClick}>Convert to Upper Case  </button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert to Lower Case  </button>
+            <button className="btn btn-danger mx-2 my-2" onClick={handleClearClick}>Clear Text  </button>
+            {/* <button className="btn btn-warning mx-2 my-2" onClick={handleEmailExtract}> Extract Email  </button> */}
            
            
         </div>
@@ -38,7 +56,7 @@ export default function TextForm(props) {
             <p>{text.split(" ").length-1} words  and  {text.length} chars </p>
             <pre>{0.008 *( text.split(" ").length-1)} Minutes Read</pre>
             <h4>Preview </h4>
-            <p>{text}</p>
+            <p className=" text-center">{text}</p>
         </div>
         </>
     )
